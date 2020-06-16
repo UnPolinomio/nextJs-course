a73c7e5185c37c8237098090678c50f71d9017ce
Hector Franco T <tor6.franco6@hotmail.com>
2020-06-16 18:13:10 -0500

    Queries in getInitialProps

--- pages/channel.jsx
+++ pages/channel.jsx
@@ -1,5 +1,114 @@
 export default class extends React.Component {
+    static async getInitialProps({ query }) {
+        let idChannel = query.id
+
+        let reqChannel = await fetch(`https://api.audioboom.com/channels/${idChannel}`)
+        let dataChannel = await reqChannel.json()
+        let channel = dataChannel.body.channel
+
+
+        let reqAudios = await fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
+        let dataAudios = await reqAudios.json()
+        let audioClips = dataAudios.body.audio_clips
+
+
+        let reqSeries = await fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`)
+        let dataSeries = await reqSeries.json()
+        let series = dataSeries.body.channels
+
+
+        return { channel, audioClips, series }
+    }
+
     render() {
-        return <h1>Vista de podcast</h1>
+        const { channel, audioClips, series } = this.props
+        return <>
+            <header>Podcast</header>
+            <h1>{channel.title}</h1>
+
+            <h2>Series</h2>
+            {series.map( ( serie ) => (
+                <div>{serie.title}</div>
+            ) ) }
+
+            <h2>Últimos podcasts</h2>
+            {audioClips.map( ( clip ) => (
+                <div>{clip.title}</div>
+            ) ) }
+
+            <style jsx>{`
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+        }
+
+        .banner {
+          width: 100%;
+          padding-bottom: 25%;
+          background-position: 50% 50%;
+          background-size: cover;
+          background-color: #aaa;
+        }
+
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+        a.channel {
+          display: block;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          width: 100%;
+        }
+        h1 {
+          font-weight: 600;
+          padding: 15px;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
+        }
+
+        .podcast {
+          display: block;
+          text-decoration: none;
+          color: #333;
+          padding: 15px;
+          border-bottom: 1px solid rgba(0,0,0,0.2);
+          cursor: pointer;
+        }
+        .podcast:hover {
+          color: #000;
+        }
+        .podcast h3 {
+          margin: 0;
+        }
+        .podcast .meta {
+          color: #666;
+          margin-top: 0.5em;
+          font-size: 0.8em;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: system-ui;
+          background: white;
+        }
+        `}</style>
+        </>
     }
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ export default class extends React.Component {
             <header>Podcast</header>
             <div className="channels">
                 { channels.map( ( channel ) => (
-                    <Link href="/channel">
+                    <Link href={`/channel?id=${channel.id}`}>
                         <a className="channel">
                             <img src={channel.urls.logo_image.original} alt=""/>
                             <h2>{channel.title}</h2>
