5e63c9f390b098ff774ce6cc00f4b3894855b482
Hector Franco T <tor6.franco6@hotmail.com>
2020-06-16 17:44:31 -0500

    Link component

---
+++ pages/channel.jsx
@@ -0,0 +1,5 @@
+export default class extends React.Component {
+    render() {
+        return <h1>Vista de podcast</h1>
+    }
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default class extends React.Component {
     static async getInitialProps() {
         let req = await fetch('https://api.audioboom.com/channels/recommended')
@@ -13,10 +15,12 @@ export default class extends React.Component {
             <header>Podcast</header>
             <div className="channels">
                 { channels.map( ( channel ) => (
-                    <div className="channel">
-                        <img src={channel.urls.logo_image.original} alt=""/>
-                        <h2>{channel.title}</h2>
-                    </div>
+                    <Link href="/channel">
+                        <a className="channel">
+                            <img src={channel.urls.logo_image.original} alt=""/>
+                            <h2>{channel.title}</h2>
+                        </a>
+                    </Link>
                 ) ) }
             </div>
 
