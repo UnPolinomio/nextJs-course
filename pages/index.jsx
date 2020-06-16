558dbca4e88e746cadd57a8fd0d4243602b31c80
Hector Franco T <tor6.franco6@hotmail.com>
2020-06-16 17:27:35 -0500

    Display channels

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,64 @@
 export default class extends React.Component {
+    static async getInitialProps() {
+        let req = await fetch('https://api.audioboom.com/channels/recommended')
+        let { body: channels } = await req.json()
+
+        return { channels }
+    }
+
     render() {
+        const { channels } = this.props
+
         return <>
-            <h1>Hello, World</h1>
-            <p>This is a <a href="#">test</a> page.</p>
-            <img src="/static/platzi-logo.png" alt="Logo"/>
+            <header>Podcast</header>
+            <div className="channels">
+                { channels.map( ( channel ) => (
+                    <div className="channel">
+                        <img src={channel.urls.logo_image.original} alt=""/>
+                        <h2>{channel.title}</h2>
+                    </div>
+                ) ) }
+            </div>
 
             <style jsx>{`
-                h1 {
-                    color: red;
+                header {
+                    color: #FFF;
+                    background: #8756ca;
+                    padding: 15px;
+                    text-align: center;
                 }
-                p :global(a) {
-                    color: green;
+
+                .channels {
+                    display: grid;
+                    grid-gap: 15px;
+                    padding: 15px;
+                    grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+                }
+                a.channel {
+                    display: block;
+                    margin-bottom: 0.5em;
+                    color: #333;
                     text-decoration: none;
                 }
-                img {
-                    max-width: 25%;
-                    margin: 0 auto;
+                .channel img {
+                    border-radius: 3px;
+                    box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                    width: 100%;
+                }
+                h2 {
+                    padding: 5px;
+                    font-size: 0.9em;
+                    font-weight: 600;
+                    margin: 0;
+                    text-align: center;
                 }
             `}</style>
 
-            <style jsx global>{`
+            <style global jsx>{`
                 body {
-                    font-family: Ubuntu, sans-serif
+                    margin: 0;
+                    font-family: system-ui;
+                    background: white;
                 }
             `}</style>
         </>
